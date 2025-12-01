9fd0e6127340b53eb30cc202f0a8660b128244f6
narawaa <nashwa.ghania@ui.ac.id>
2025-12-02 02:15:20 +0700

    feat: add query url

--- kagebunshin/urls.py
+++ kagebunshin/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('search/', include('search.urls')),
+    path('query/', include('query.urls')),
 ]
