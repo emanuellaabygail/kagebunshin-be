04a0b5ab3d1d33ebb5a07ebd24d4fa380bacb599
narawaa <nashwa.ghania@ui.ac.id>
2025-12-02 02:15:51 +0700

    feat: add views example

---
+++ search/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from search import views
+from search.views import get_data
+
+app_name = 'search'
+urlpatterns = [
+    path('get-data/', get_data, name='get_example_data'),
+]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from api.sparql_client import run_sparql
+from api.views import sparql_to_json
+from kagebunshin.common.utils import api_response
 
-# Create your views here.
+# pakai run_sparql dari api/sparql_client.py untuk ambil data dari GraphDB
+# pakai sparql_to_json dari api/views.py untuk ratain (mempermudah) hasil SPARQL ke JSON biasa
+# return dibungkus pake api_response dari kagebunshin/common/utils.py biar konsisten
+# jangan lupa bikin .env
+
+@api_view(['GET'])
+def get_data(request):
+    query = """
+    SELECT ?s ?p ?o
+    WHERE {
+      ?s ?p ?o
+    }
+    LIMIT 10
+    """
+    result = run_sparql(query) 
+
+    if "error" in result:
+        return api_response(status.HTTP_500_INTERNAL_SERVER_ERROR, "Gagal ambil data", result)
+    
+    data = sparql_to_json(result)
+    return api_response(status.HTTP_200_OK, "Berhasil ambil data", data)
